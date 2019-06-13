a0570e6c8ce465f13bcdaad95559146dc6facf6c
Daniel H. G. Mescoloto <dan.gasperoni@gmail.com>
2019-06-13 10:51:39 -0300

    Initial Models

---
+++ src/Services/Catalog/Catalog.API/Model/CatalogItem.cs
@@ -0,0 +1,22 @@
+﻿namespace Catalog.API.Model
+{
+    public class CatalogItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Sku { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal PricingList { get; set; }
+
+        public decimal? PricingSale { get; set; }
+
+        public int CatalogTypeId { get; set; }
+
+        public CatalogType CatalogType { get; set; }
+
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Model/CatalogType.cs
@@ -0,0 +1,13 @@
+﻿namespace Catalog.API.Model
+{
+    public class CatalogType
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+
+    }
+}
